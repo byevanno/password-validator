ec58f2f81f54a4b61b3d49c2d3f120ab6a5eaf56
byevanno <divannodavis@hotmail.com>
2018-06-09 21:22:09 -0300

    part 1 done

--- app/src/main/java/com/example/vanno/password_validator/Validator.java
+++ app/src/main/java/com/example/vanno/password_validator/Validator.java
@@ -1,4 +1,47 @@
 package com.example.vanno.password_validator;
 
+
 public class Validator {
+
+    public Validator()
+    {
+    }
+
+    private String password;
+
+    public void enter_password(String pass)
+    {
+        password = pass;
+    }
+
+    public String get_password()
+    {
+        return password;
+    }
+
+    public int validate(String p)
+    {
+        return(rule1(p)+ rule2(p));
+    }
+    //checks if password is longer than 7 characters
+    private int rule1(String p)
+    {
+        if(p.length() >7)
+        {
+            //returns 10 if true to keep track if this rule
+            //or the other rule was broken
+            return 10;
+        }
+        else return 0;
+    }
+    private int rule2(String p)
+    {
+        if(!p.matches("password"))
+        {
+            //returns 1 if true to keep track if this rule
+            //or the other rule was broken
+            return 1;
+        }
+        else return 0;
+    }
 }
